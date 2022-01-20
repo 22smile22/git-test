e6a2c8d53f2f32c86863339f99649e3eded44ac8
22smile22 <96616073+22smile22@users.noreply.github.com>
2022-01-20 20:09:07 +0600

    main.py changes

--- main2.py
+++ main2.py
@@ -1,8 +1,15 @@
 """
+Первые три команды пишутся только один раз когда вы делаете первый пуш
 git --version
 git config --global user.name "YOUR USERNAME ON GITHUB"
 git config --global user.email "YOUR EMAIL ON GITHUB"
-git init
+*
+git init (для нового проекта)
 git add .
+git status
+
+git remote add origin <HTTPS link.git> (Ремоте тоже пишется единожды когда соединяем проект с репозиторием)
+git branch
+
 
 """
